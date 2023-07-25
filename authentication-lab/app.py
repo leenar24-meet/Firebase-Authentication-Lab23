38a0c4d69cd87354d0ee447db003f3669ae4f7cb
leenar24-meet <139689053+leenar24-meet@users.noreply.github.com>
2023-07-25 11:02:23 +0300

    this is not finished but i have to start the project

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 config = {
-  "apiKey": "AIzaSyA-_rx6KSqW8BQTikNlmMfqYBVdGgu4BGU",
-  "authDomain": "winter-project-31d88.firebaseapp.com",
-  "projectId": "winter-project-31d88",
-  "storageBucket": "winter-project-31d88.appspot.com",
-  "messagingSenderId": "19928945824",
-  "appId": "1:19928945824:web:2a205188237a97b2c065c5",
-  "measurementId": "G-6FEGKH25VF",
-  "databaseURL":"https://winter-project-31d88-default-rtdb.europe-west1.firebasedatabase.app/" };
+  "apiKey": "AIzaSyBCmZHuYpQFMPyopY9ogFkxKAX4quLB978",
+  "authDomain": "allawi-11c42.firebaseapp.com",
+  "projectId": "allawi-11c42",
+  "storageBucket": "allawi-11c42.appspot.com",
+  "messagingSenderId": "1070532889920",
+  "appId": "1:1070532889920:web:fcd69c7fbc34108c0ffea5",
+  "measurementId": "G-L4EZSYCHBB",
+  "databaseURL":"https://allawi-11c42-default-rtdb.firebaseio.com/" };
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -18,19 +18,19 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
-
+# fix the login
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    if request.method== 'POST':
-        email = request.form['email']
-        Password = request.form['password']
-        try:
-            login_session['user'] =auth.signin_user_with_email_and_password(email, password)
+    try:
+        if request.method== 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             return redirect(url_for('add_tweet'))
-        except: 
-            print('the key user isn’t in login_session')
-            return redirect(url_for('signin'))
+    except: 
+        print("the key user isn’t in login_session")
+        return redirect(url_for('signin'))
     return render_template("signin.html")
 
 
@@ -45,26 +45,35 @@ def signup():
             user= {"full_name": request.form['full_name'], "username": request.form['username'], "bio": request.form['bio']}
             db.child("Users").child(UID).set(user)
             return redirect(url_for('add_tweet'))
-        except: 
-            print('the key user isn’t in login_session')
-            exit()
-            return redirect(url_for('signin'))
-
+        except Exception as e: 
+            print(e)
 
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    if method=="POST":
+    if request.method=="POST":
         try: 
             UID = login_session['user']['localId']
-            tweets={"title": request.form['Title'], "text": request.form['Text']}
-
-
-
-
+            tweets={"title": request.form['Title'], "text": request.form['Text'], "UID":UID}
+            db.child("Tweets").push('tweets')
+        except:
+            return redirect(url_for('signin'))
     return render_template("add_tweet.html")
+
+@app.route('/all_tweets')
+def all_tweets():
+    alltweets= db.child("tweets").get().val()
+    return render_template("tweets.html")
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
